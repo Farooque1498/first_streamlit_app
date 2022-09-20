71f89fd91ce4d40b568aaf11cae2782ea45c09ae
Mohammed Farooque Kaleem Shaikh <113969598+Farooque1498@users.noreply.github.com>
2022-09-20 15:33:26 +0530

    Update streamlit_app.py
    added another title
    added new header
    added 4 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.title('My Moms New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
