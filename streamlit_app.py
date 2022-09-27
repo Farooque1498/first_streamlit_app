e1733d4a0e0d28b64827af870d439937633490ca
Mohammed Farooque Kaleem Shaikh <113969598+Farooque1498@users.noreply.github.com>
2022-09-27 19:46:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,6 +80,13 @@ if streamlit.button('Add a Fruit to the List'):
     streamlit.text(back_from_function)
     my_cnx.close()
      
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 
